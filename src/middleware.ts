f50de767285858798853b4b3667bd8c28621c2e1
Shwetanshu13 <shwetanshu@shuniyavigyan.com>
2024-08-11 22:27:56 +0530

    Added route protection for forgot pass and verify email

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,37 +1,46 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { getToken } from 'next-auth/jwt';
-export { default } from 'next-auth/middleware';
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+export { default } from "next-auth/middleware";
 
 export const config = {
-    matcher: ['/home', '/recipes', '/recipe:path*', '/add-recipe', '/auth/signup', '/auth/login', '/'],
+  matcher: [
+    "/home",
+    "/recipes",
+    "/recipe:path*",
+    "/add-recipe",
+    "/auth/signup",
+    "/auth/login",
+    "/auth/verify-email",
+    "/auth/forgot-password",
+    "/",
+  ],
 };
 
 export async function middleware(request: NextRequest) {
-    const token = await getToken({ req: request });
-    // console.log('token', token);
-    const url = request.nextUrl;
+  const token = await getToken({ req: request });
+  // console.log('token', token);
+  const url = request.nextUrl;
 
-    if (
-        token &&
-        (
-            url.pathname.startsWith('/auth/signup') ||
-            url.pathname.startsWith('/auth/login') ||
-            url.pathname === '/'
-        )
-    ) {
-        return NextResponse.redirect(new URL('/home', request.url));
-    }
+  if (
+    token &&
+    (url.pathname.startsWith("/auth/signup") ||
+      url.pathname.startsWith("/auth/login") ||
+      url.pathname.startsWith("/auth/verify-email") ||
+      url.pathname.startsWith("/auth/forgot-password") ||
+      url.pathname === "/")
+  ) {
+    return NextResponse.redirect(new URL("/home", request.url));
+  }
 
-    if (!token &&
-        (
-            url.pathname.startsWith('/recipes') ||
-            url.pathname.startsWith('/home') ||
-            url.pathname.startsWith('/recipe') ||
-            url.pathname.startsWith('/add-recipe')
-        )
-    ) {
-        return NextResponse.redirect(new URL('/auth/signup', request.url));
-    }
+  if (
+    !token &&
+    (url.pathname.startsWith("/recipes") ||
+      url.pathname.startsWith("/home") ||
+      url.pathname.startsWith("/recipe") ||
+      url.pathname.startsWith("/add-recipe"))
+  ) {
+    return NextResponse.redirect(new URL("/auth/signup", request.url));
+  }
 
-    return NextResponse.next();
+  return NextResponse.next();
 }
